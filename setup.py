cbd021bc5e6c04e87d36aaffe2934f207ab3600e
Tom von Schwerdtner <tvon@calculon.local>
2009-04-11 14:42:43 -0400

    moving templates to the package (which is a bit more django-like anyway), and fixing the installation to put them int he right place

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup, Command
+from distutils.command.install import INSTALL_SCHEMES
+
 import os
 
 try:
@@ -31,17 +33,38 @@ class PyTestCommand(Command):
         exitstatus = session.main()
         config.pytestplugins.do_unconfigure(config)
         raise SystemExit(exitstatus)
-                
 
-setup(name='hyde',
-      version='0.3b',
-      author='Lakshmi Vyas',
-      author_email='lakshmi.vyas@gmail.com',
-      url='http://www.ringce.com/products/hyde/hyde.html',
-      description='Static website generator using Django templates',
 
-      packages=['hydeengine'],
-      scripts=['scripts/hyde.py',],
-      
-      cmdclass = {'test': PyTestCommand},
-      )
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+data_files = []
+for dirpath, dirnames, filenames in os.walk('hydeengine/templates'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): 
+            del dirnames[i]
+    if filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+#import pprint
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(data_files)
+#import sys
+#sys.exit(1)
+
+setup(
+    name='hyde',
+    version='0.3b',
+    author='Lakshmi Vyas',
+    author_email='lakshmi.vyas@gmail.com',
+    url='http://www.ringce.com/products/hyde/hyde.html',
+    description='Static website generator using Django templates',
+
+    packages=['hydeengine'],
+    scripts=['scripts/hyde.py',],
+      data_files=data_files,
+#    package_data={'templates': ['templates/*']},
+
+    cmdclass = {'test': PyTestCommand},
+    )
