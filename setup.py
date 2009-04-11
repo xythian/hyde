f5a0db320c7018aa3c4d976d56ba9a0384c52c72
Tom von Schwerdtner <tvon@calculon.local>
2009-04-11 14:00:21 -0400

    testing works, though I need to examine what a few of those lines actually do since I basically copied the py.test runner

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup, Command
 import os
 
-class TestCommand(Command):
-    description = 'Run package tests'
+try:
+    import py
+except ImportError:
+    py = False
+
+class PyTestCommand(Command):
+    description = 'Run package tests with py.test'
     user_options = []
     
     def initialize_options(self):
@@ -12,7 +17,20 @@ class TestCommand(Command):
         pass
         
     def run(self):
-        print "I don't actually know how to run these tests yet, so..."
+        if not py:
+            print "Tests require py lib:  http://codespeak.net/py/dist/download.html"
+            return
+        
+        tests = ['tests/%s' % t for t in os.listdir('tests') if t.endswith('.py') and t.startswith('test_')]
+
+        # This bit is more or less copied from  the py.test runner in py/test/cmdline.py
+        config = py.test.config
+        config.parse(tests) 
+        config.pytestplugins.do_configure(config)
+        session = config.initsession()
+        exitstatus = session.main()
+        config.pytestplugins.do_unconfigure(config)
+        raise SystemExit(exitstatus)
                 
 
 setup(name='hyde',
@@ -25,5 +43,5 @@ setup(name='hyde',
       packages=['hydeengine'],
       scripts=['scripts/hyde.py',],
       
-      cmdclass = {'test': TestCommand},
+      cmdclass = {'test': PyTestCommand},
       )
