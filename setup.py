1192f0b398f158b3451560799c459376b8365d43
Tom von Schwerdtner <tvon@calculon.local>
2009-04-11 12:01:09 -0400

    rearranging things.. don't think the lib/ stuff should be installed with the package so I'm moving it to contrib/

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 
 setup(name='hyde',
-      version='0.3 Beta',
+      version='0.3b',
+      author='Lakshmi Vyas',
+      author_email='lakshmi.vyas@gmail.com',
+      url='http://www.ringce.com/products/hyde/hyde.html',
+      description='Static website generator using Django templates',
+      
       packages=['hydeengine'],
-      scripts=['scripts/hyde.py',]
+      scripts=['scripts/hyde.py',],
       )
