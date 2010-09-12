d87de2258141f9d6b7fc2ed97936d1165dbf2437
Ken <fox@mars.org>
2010-09-11 18:14:41 -0700

    Change default YUI Compressor to match the version checked in (2.4.2).

--- templates/default/settings.py
+++ templates/default/settings.py
@@ -112,7 +112,7 @@ GROWL = None
 # path for YUICompressor, or None if you don't
 # want to compress JS/CSS. Project homepage:
 # http://developer.yahoo.com/yui/compressor/
-YUI_COMPRESSOR = "./lib/yuicompressor-2.4.1.jar"
+YUI_COMPRESSOR = "./lib/yuicompressor-2.4.2.jar"
 #YUI_COMPRESSOR = None 
 
 # path for Closure Compiler, or None if you don't
